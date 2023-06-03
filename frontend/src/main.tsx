2e1152d5d6ba11f2fc248ff30e946a1910f986a6
avanticoders <avanticoders@gmail.com>
2023-06-03 23:20:48 +0300

    Update import of bootstrap both locally and online

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import "/home/deluxe/Downloads/Compressed/bootstrap-5.0.2-dist/css/bootstrap.min.css";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css";
+
+// LOCAL BOOTSTRAP
+import "/home/deluxe/Downloads/Compressed/bootstrap-5.0.2-dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
